0f73b909571121cfb9bc9b5875f69140393ea41c
Maor-Katz <38643430+Maor-Katz@users.noreply.github.com>
2019-04-21 18:52:42 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,27 +6,53 @@ import ClothesList from "./ClothesList";
 import Basket from "./Basket";
 import {addList, addToBasket, openOrCloseBasket} from "./redux/actions/action";
 import {connect} from "react-redux";
+import BackToTop from "react-back-to-top-button";
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {scrollUp:true}
+
+    }
+    backToTop = {
+        backgroundColor: '#039be5',
+        borderRadius:'20px',
+        width:'150px',
+        height:'50px',
+        fontSize:'18px',
+        top:'20px',
+        left:'17%',
+        color:'white'
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
     }
 
     render() {
         const {isBasketTime, openOrCloseBasket} = this.props
+        const {scrollUp} = this.state
         return (
 
             <div className="App">
-                <div className="App-header">
 
+                <div className="App-header">
                     <h1>MK APP</h1>
                 </div>
+                <BackToTop
+                    showAt={100}
+                    speed={1500}
+                    easing="easeInOutQuint"
+                    showOnScrollUp={false}
+                    style={this.backToTop}
+                >
+                    <span>Back to Top</span>
+                </BackToTop>
                 {this.props.todos}
                 <div className="container">
                     <div className="sizesContainer">
                         < ChooseSize/>
-                        <button className="goToBasket" onClick={() => openOrCloseBasket(true)}>Go to the Basket Dudes
+                        <button className="goToBasket" onClick={() => openOrCloseBasket(true)}>Open basket
                         </button>
                     </div>
                     <div className="listConatainer">
@@ -39,6 +65,7 @@ class App extends Component {
 
                 </div>}
                 < IconComponent/>
+
             </div>
         );
     }
